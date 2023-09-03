22c02ebb62c48b291edd9d83c299dc3dcbdcc042
Felipe <felipe.vkw@hotmail.com>
2023-09-03 16:34:59 -0300

    Atualizando

---
+++ aula42.py
@@ -0,0 +1,5 @@
+frase = """O python é uma linguagem de programação multiparadigma. 
+Pyhton foi criado por Guido Van Rossum em 1991."""
+
+quantidade = frase.count("python")
+print(quantidade)
--- testes.py
+++ testes.py
@@ -4,8 +4,14 @@ from selenium.webdriver.chrome.service import Service
 # Instanciar web driver
 driver = webdriver.Chrome(service=Service())
 # abrir site google
+
+#%%
 driver.get('https://www.google.com.br/')
 
 webdriver_version = driver.capabilities['browserVersion']
 
 print("Versão do WebDriver:", webdriver_version)
+
+#%%
+driver.get('https://www.youtube.com.br/')
+
